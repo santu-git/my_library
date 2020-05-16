fa563cb415522dd8ab97bbcb89b314c1b9f99a4a
Santu Koley <santu.cst08@gmail.com>
2020-05-16 13:10:58 +0530

    Test Data and Author model unit tese cases

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save author without first_name" do
+    author = authors(:author_1) # loading first record from author.yml
+    author.first_name = ''
+    assert_not author.save # author.save should return false or nil
+  end
+
+  test 'should return validation error message if first_name is blank' do
+    author = authors(:author_1)
+    author.first_name = ''
+    author.save
+    assert_includes(author.errors[:first_name], "can't be blank")
+  end
+
+  test "should not save author without last_name" do
+    author = authors(:author_1)
+    author.last_name = ''
+    assert_not author.save
+  end
+
+  test 'should return validation error message if last_name is blank' do
+    author = authors(:author_1)
+    author.last_name = ''
+    author.save
+    assert_includes(author.errors[:last_name], "can't be blank")
+  end
+
+  test "should save a valid record" do
+    author = authors(:author_1)
+    assert author.save
+  end
+
+  test "class method total_count shoud exist" do
+    assert_respond_to Author, :total_count
+  end
+
+  test "total_count should return proper count" do
+    assert_equal authors.count, Author.total_count
+  end
+
+  test "instance method short_name should exist" do
+    author = authors(:author_20) # loading 20th record from author.yml
+    assert_respond_to author, :short_name
+  end
+
+  test "instance method short_name should return proper short_name" do
+    author = authors(:author_20) # loading 20th record from author.yml
+    assert_equal "#{author.first_name.chars.first}. #{author.last_name}", author.short_name
+  end
 end
